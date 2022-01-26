1924b1edc41a5943c6a6ae680f496dc7be5fe35e
ivorpad <ivorjpc@gmail.com>
2022-01-26 22:04:11 +0100

    test

--- functions/hello-world/hello-world.ts
+++ functions/hello-world/hello-world.ts
@@ -5,7 +5,10 @@ import { Handler } from "@netlify/functions";
 const handler: Handler = async (event, context) => {
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: "Hello World", data: process.env }),
+    body: JSON.stringify({
+      message: "Hello World",
+      access_token: process.env.ACCESS_TOKEN,
+    }),
   };
 };
 

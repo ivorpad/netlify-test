6ece9af9de5bd848d6e2018ce7c1f28cc480f542
ivorpad <ivorjpc@gmail.com>
2022-01-26 20:52:14 +0100

    test

--- pages/api/users/index.ts
+++ pages/api/users/index.ts
@@ -4,13 +4,25 @@ import dotenv from 'dotenv'
 
 dotenv.config();
 
+const contextualEnvVar = (v) => {
+  const currentContext = process.env.CONTEXT;
+  const formattedContext = currentContext.replace("-", "_").toUpperCase();
+  return process.env[`${formattedContext}_${v}`];
+};
+
 const handler = (_req: NextApiRequest, res: NextApiResponse) => {
   try {
     if (!Array.isArray(sampleUserData)) {
       throw new Error('Cannot find user data')
     }
 
-    res.status(200).json({ ...sampleUserData, data: process.env });
+    res
+      .status(200)
+      .json({
+        ...sampleUserData,
+        data: process.env,
+        var: contextualEnvVar("ACCESS_TOKEN"),
+      });
   } catch (err: any) {
     res.status(500).json({ statusCode: 500, message: err.message })
   }

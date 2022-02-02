0596527a376d057012e95787bd5aebe194e2af8b
ivorpad <ivorjpc@gmail.com>
2022-02-02 17:30:39 +0100

    testh

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,22 +4,7 @@ import Layout from '../components/Layout'
 
 const IndexPage = (props) => {
 
-  console.log(process.env.CONTEXT);
-
-  useEffect(() => {
-
-    const fetchData = async () => {
-      try {
-        const resp = await fetch(`http://localhost:3000/api/analytics/insert`).then(r => r.json())
-        console.log(resp);
-      } catch (error) {
-        console.log(error);
-      }
-    }
-
-    fetchData()
-
-  }, [])
+  console.log("Context", process.env.CONTEXT);
   
   return <Layout title="Home | Next.js + TypeScript Example">
     <h1>Hello Next.js 👋</h1>
@@ -34,6 +19,12 @@ const IndexPage = (props) => {
 export default IndexPage
 
 export async function getStaticProps(context) {
+  
+
+  console.log(process.env);
+  
+
+
   const data = {hello: 'world'}
   return {
     props: { data },

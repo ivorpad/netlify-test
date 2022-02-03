84b0726073d64e901c78e8f9741cf56b14ab3048
ivorpad <ivorjpc@gmail.com>
2022-02-03 18:02:29 +0100

    test

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ module.exports = {
   env: {
     LAMBDA_URL: process.env.LAMBDA_URL,
     GLOBAL_ALL_ENVS: process.env.GLOBAL_ALL_ENVS,
-    URL: process.env.URL,
+    MY_URL: process.env.MY_URL,
+    DEPLOY_PRIME_URL: process.env.DEPLOY_PRIME_URL,
   },
 };
